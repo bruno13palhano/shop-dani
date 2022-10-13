7032646ed4439a9d59ead0d6e8080cf5121cfe62
bruno13palhano <bruno13palhano@gmail.com>
2022-10-12 23:29:51 -0300

    some tests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from './product.service';
+import { Product } from './model/product';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'shope-dani';
+  products?: Product[];
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.getProducts(0, 2);
+  }
+
+  getProducts(startAt: number, productsPerQuery: number) {
+    this.productService.getProductsOrderByIdDesc(startAt, productsPerQuery)
+      .subscribe(products => this.products = products);
+  }
 }
