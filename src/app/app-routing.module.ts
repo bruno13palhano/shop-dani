2387693b6ab7458f609e2456af4280b7a5812280
bruno13palhano <bruno13palhano@gmail.com>
2022-10-14 22:39:13 -0300

    add new routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainMenuComponent } from './main-menu/main-menu.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductComponent } from './product/product.component';
+import { ProductsCategoriesComponent } from './products-categories/products-categories.component';
 
 const routes: Routes = [
-  {path: 'test', component: MainMenuComponent}
+  { path: '', component: ProductComponent},
+  { path: 'categories', component: ProductsCategoriesComponent},
+  { path: 'detail', component: ProductDetailComponent}
 ];
 
 @NgModule({
