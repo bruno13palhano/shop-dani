2b744c755eab4534ea3c7c2515958cc6873717f8
bruno13palhano <bruno13palhano@gmail.com>
2022-11-01 19:13:40 -0300

    added search

--- src/app/main-menu/main-menu.component.ts
+++ src/app/main-menu/main-menu.component.ts
@@ -1,5 +1,8 @@
+import { state } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Product } from '../model/product';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-main-menu',
@@ -8,19 +11,50 @@ import { Router } from '@angular/router';
 })
 export class MainMenuComponent implements OnInit {
 
-  search: String = "";
+  search: string = "";
+  casaProducts?: Product[];
 
-  constructor(private router: Router) { }
+  constructor(private router: Router,
+              private casaService: ProductService) { }
 
   ngOnInit(): void {
   }
 
   goHome() {
-    this.router.navigateByUrl('/');
+    this.router.navigate(['/']);
   }
 
   goCategories() {
-    // console.log('fui clicado!');
-    this.router.navigateByUrl('/categories');
+    this.router.navigate(['/categories']);
+  }
+
+  goBlog() {
+    let product: Product = {productId: 1,
+                            productName: 'carrinho',
+                            productDescription: 'carrinho de madeira',
+                            productPrice: 34.5,
+                            productImageUrl: '',
+                            productLink: '', }
+
+    // this.router.navigateByUrl('', {skipLocationChange: true } ).then(() => {
+    //   this.router.navigateByUrl('/detail', {state: product});
+    // });
+  }
+
+  doSearch(searchUrl: string) {
+      this.casaService.doSearch(searchUrl).
+        subscribe(casaProducts => {
+          this.casaProducts = casaProducts;
+
+          this.router.navigateByUrl('', { 
+            skipLocationChange: true
+           
+          }).then(() => {
+            console.log(casaProducts);
+            this.router.navigateByUrl('/search-products', {
+              state: casaProducts
+            });
+          });
+        });
   }
 }
