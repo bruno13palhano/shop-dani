57e6279432abe9157883d5ed1084407f968747cf
bruno13palhano <bruno13palhano@gmail.com>
2022-11-01 19:18:37 -0300

    added new components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainMenuComponent } from './main-menu/main-menu.component';
+import { ProductCarouselComponent } from './product-carousel/product-carousel.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductSearchListComponent } from './product-search-list/product-search-list.component';
 import { ProductComponent } from './product/product.component';
 import { ProductsCategoriesComponent } from './products-categories/products-categories.component';
 
 const routes: Routes = [
-  { path: '', component: ProductComponent},
-  { path: 'categories', component: ProductsCategoriesComponent},
-  { path: 'detail', component: ProductDetailComponent}
+  { path: 'product-list', component: ProductListComponent },
+  { path: 'categories', component: ProductsCategoriesComponent },
+  { path: 'detail', component: ProductDetailComponent },
+  { path: 'carousel', component: ProductCarouselComponent },
+  // { path: '', redirectTo: 'categories', pathMatch: 'full'},
+  { path: '', component: ProductListComponent },
+  { path: 'search-products', component: ProductSearchListComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {scrollPositionRestoration: 'enabled'})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
