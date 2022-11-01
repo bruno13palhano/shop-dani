147e6bc8d709536b9f756b49c601b495a4744c33
bruno13palhano <bruno13palhano@gmail.com>
2022-11-01 19:22:46 -0300

    added new services

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -4,6 +4,9 @@ import { HttpClient} from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 const productUrl = 'http://localhost:8080/product/';
+const perfumeUrl = 'http://localhost:8080/perfume/';
+const casaUrl = 'http://localhost:8080/casa/';
+const searchUrl = 'http://localhost:8080/search/';
 
 @Injectable({
   providedIn: 'root'
@@ -40,4 +43,76 @@ export class ProductService {
     return this.http.get<Product[]>(productUrl+'price-desc/?params='+startAt+','+productsPerQuery);
   }
 
+
+
+  getPerfumesById(productId: number): Observable<Product> {
+    return this.http.get<Product>(perfumeUrl+productId)
+  }
+
+  getPerfumesOrderByIdAsc(startAt: number, productsPerQuery: number): Observable<Product[]> {
+    return this.http.get<Product[]>(perfumeUrl+'id-asc/?params='+startAt+','+productsPerQuery);
+  }
+
+  getPerfumesOrderByIdDesc(startAt: number, productsPerQuery: number): Observable<Product[]> {
+    return this.http.get<Product[]>(perfumeUrl+'id-desc/?params='+startAt+','+productsPerQuery);
+  }
+
+  getPerfumesOrderByNameAsc(startAt: number, productsPerQuery: number): Observable<Product[]> {
+    return this.http.get<Product[]>(perfumeUrl+'name-asc/?params='+startAt+','+productsPerQuery);
+  }
+
+  getPerfumesOrderByNameDesc(startAt: number, productsPerQuery: number): Observable<Product[]> {
+    return this.http.get<Product[]>(perfumeUrl+'name-desc/?params='+startAt+','+productsPerQuery);
+  }
+
+  getPerfumesOrderByPriceAsc(startAt: number, productsPerQuery: number): Observable<Product[]> {
+    return this.http.get<Product[]>(perfumeUrl+'price-asc/?params='+startAt+','+productsPerQuery);
+  }
+
+  getPerfumesOrderByPriceDesc(startAt: number, productsPerQuery: number): Observable<Product[]> {
+    return this.http.get<Product[]>(perfumeUrl+'price-desc/?params='+startAt+','+productsPerQuery);
+  }
+
+  doPerfumeSearch(queryParam: string): Observable<Product[]> {
+    return this.http.get<Product[]>(perfumeUrl+'search/?params='+queryParam);
+  }
+
+
+
+
+  getCasaById(productId: number): Observable<Product> {
+    return this.http.get<Product>(casaUrl+productId);
+  }
+
+  getCasaOrderByIdAsc(startAt: number, productsPerQuery: number): Observable<Product[]> {
+    return this.http.get<Product[]>(casaUrl+'id-asc/?params='+startAt+','+productsPerQuery);
+  }
+
+  getCasaOrderByIdDesc(startAt: number, productsPerQuery: number): Observable<Product[]> {
+    return this.http.get<Product[]>(casaUrl+'id-desc/?params='+startAt+','+productsPerQuery);
+  }
+
+  getCasaOrderByNameAsc(startAt: number, productsPerQuery: number): Observable<Product[]> {
+    return this.http.get<Product[]>(casaUrl+'name-asc/?params='+startAt+','+productsPerQuery);
+  }
+
+  getCasaOrderByNameDesc(startAt: number, productsPerQuery: number): Observable<Product[]> {
+    return this.http.get<Product[]>(casaUrl+'name-desc/?params='+startAt+','+productsPerQuery);
+  }
+
+  getCasaOrderByPriceAsc(startAt: number, productsPerQuery: number): Observable<Product[]> {
+    return this.http.get<Product[]>(casaUrl+'price-asc/?params='+startAt+','+productsPerQuery);
+  }
+
+  getCasaOrderByPriceDesc(startAt: number, productsPerQuery: number): Observable<Product[]> {
+    return this.http.get<Product[]>(casaUrl+'price-desc/?params='+startAt+','+productsPerQuery);
+  }
+
+  doCasaSearch(queryParam: string): Observable<Product[]> {
+    return this.http.get<Product[]>(casaUrl+'search/?params='+queryParam);
+  }
+
+  doSearch(queryParam: string): Observable<Product[]> {
+    return this.http.get<Product[]>(searchUrl+'?params='+queryParam);
+  }
 }
